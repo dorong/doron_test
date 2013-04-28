c9d9c32cca26fa4f2aeef5e870216f0fa579c020
Doron Gill <doron@stratoscale.com>
2013-04-28 14:54:32 +0300

    test pull 2

--- hello.c
+++ hello.c
@@ -4,5 +4,6 @@ main ()
 {
   printf("Oh No!!! Cube Test, my name is Simba\n");
   printf("adding now another line sp that I can test the pull request\n");
+  printf("adding yet another line\n");
 }
 
