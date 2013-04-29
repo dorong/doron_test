df924f2a07ed6b91d9904ddffe2082e305c503ba
Doron Gill <doron@stratoscale.com>
2013-04-29 18:02:56 +0300

    hopefully installing the env will work now

--- hello.c
+++ hello.c
@@ -6,5 +6,6 @@ main ()
   printf("adding now another line sp that I can test the pull request\n");
   printf("adding yet another line\n");
   printf("and now the Bamboo line");
+  printf("another build for installing the environment");
 }
 
