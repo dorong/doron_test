96f44b61f12a81c2532027077d07d0c350afdb88
Bam Boo <bamboo@stratoscale.com>
2013-04-29 12:22:55 +0300

    from Bamboo

--- hello.c
+++ hello.c
@@ -5,5 +5,6 @@ main ()
   printf("Oh No!!! Cube Test, my name is Simba\n");
   printf("adding now another line sp that I can test the pull request\n");
   printf("adding yet another line\n");
+  printf("and now the Bamboo line");
 }
 
